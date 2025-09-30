c67c300cf0c6995c6c5b0c728c26372d68406853
Yash Yenurkar <yash.yenurkar@oracle.com>
2025-10-01 01:17:56 +0530

    Prometheus and Grafana

--- anomaly-worker/src/main.py
+++ anomaly-worker/src/main.py
@@ -5,13 +5,22 @@ from src.detector import MovingAverageDetector
 from src.ml_detector import IsolationForestDetector
 from src.notifier import Notifier
 from dotenv import load_dotenv
+from prometheus_client import start_http_server, Gauge, Counter
 
 BROKER = "127.0.0.1:9092"
 TOPIC = os.getenv("KAFKA_TOPIC", "metrics")
 GROUP_ID = "anomaly-worker"
 
+cpu_usage_gauge = Gauge('cpu_usage_percent', 'Current CPU Usage in %')
+anomaly_gauge = Gauge('cpu_anomaly_detected', 'Anomaly Detected (1 for yes, 0 for no)')
+anomaly_counter = Counter('cpu_anomaly_total', 'Number of anomalies detected')
+
 def run():
     load_dotenv() 
+
+    start_http_server(8000)
+    print("[Prometheus] Metrics server started at http://localhost:8000/metrics")
+
     consumer = create_consumer(BROKER, GROUP_ID, TOPIC)
     # Choose which detector to use
     use_ml = os.getenv("USE_ISOLATION_FOREST", "false").lower() == "true"
@@ -41,14 +50,25 @@ def run():
                 alert_msg = f"Anomaly detected (CPU={a})"
                 print("[ALERT]", alert_msg)
                 notifier.notify(alert_msg)
+                cpu_usage_gauge.set(cpu)
+                anomaly_gauge.set(1)
+                anomaly_counter.inc()
+            if not anomalies:
+                print("[OK]", metric)
+                cpu_usage_gauge.set(cpu)
+                anomaly_gauge.set(0)
         else:
             if detector.detect(cpu):
                 alert_msg = f"Anomaly detected: {metric}"
                 print("[ALERT]", alert_msg)
                 notifier.notify(alert_msg)
+                cpu_usage_gauge.set(cpu)
+                anomaly_gauge.set(1)
+                anomaly_counter.inc()
             else:
                 print("[OK]", metric)
+                cpu_usage_gauge.set(cpu)
+                anomaly_gauge.set(0)
     consumer.close()
-
 if __name__ == "__main__":
     run()
